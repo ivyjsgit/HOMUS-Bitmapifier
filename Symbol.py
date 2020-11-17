8c2ebddb6f35a5cea31f61fb9924ce436484b58c
Ivy <ivyjs@hendrix.edu>
2020-11-16 18:16:39 -0600

    Removed Line class for better use with mapping

--- Symbol.py
+++ Symbol.py
@@ -1,7 +1,8 @@
-from Line import *
 from Point import *
 import gizeh
 import os
+import cv2
+
 
 class Symbol:
     name = "Unnamed Symbol"
@@ -22,18 +23,30 @@ class Symbol:
             for i in range(0,len(splitted_by_semicolon)-1):
                 current_pair = splitted_by_semicolon[i].split(",")
                 next_pair = splitted_by_semicolon[i+1].split(",")
-                line = Line(Point(current_pair[0],current_pair[1]), Point(next_pair[0],next_pair[1]))
+                point_1 = (current_pair[0],current_pair[1])
+                point_2  = (next_pair[0],next_pair[1])
+                lines.append(point_1)
+                lines.append(point_2)
+                # line = Line(Point(current_pair[0],current_pair[1]), Point(next_pair[0],next_pair[1]))
 
-                lines.append(line)
-
+                # lines.append(line)
+        # print(lines)
         return lines
 
     def draw(self,filename_to_write):
         surface = gizeh.Surface(width=300, height=300)
         square = gizeh.square(l=600, fill=(1,1,1), xy=(0,0))
         square.draw(surface)
-        for line in self.lines:
-            line_to_draw = gizeh.polyline(points=[(int(line.point_1.x),int(line.point_1.y)),(int(line.point_2.x),int(line.point_2.y))],stroke_width=3, stroke=(0,0,0), fill=(1,1,1))
+        if len(self.lines)%2!=0:
+            self.lines.pop()
+        for i in range(0,len(self.lines),2):
+            print(self.lines)
+            print(f"Trying {i}")
+            point_1 = self.lines[i]
+            point_2 = self.lines[i+1]
+            points=[(int(point_1[0]),int(point_1[1])),(int(point_2[0]),int(point_2[1]))]
+            line_to_draw = gizeh.polyline(points,stroke_width=3, stroke=(0,0,0), fill=(1,1,1))
+            # line_to_draw = gizeh.polyline(points=[(int(line.point_1.x),int(line.point_1.y)),(int(line.point_2.x),int(line.point_2.y))],stroke_width=3, stroke=(0,0,0), fill=(1,1,1))
             line_to_draw.draw(surface)
-        
+        # cv2.imshow('image', surface.get_npimage())
         surface.write_to_png(f"{filename_to_write}.png")
--- bitmapifier.py
+++ bitmapifier.py
@@ -3,7 +3,7 @@ from Symbol import *
 from tkinter import *
 from tkinter.ttk import *
 import os
-import tensorflow as tf
+# import tensorflow as tf
 
 
 
@@ -11,12 +11,12 @@ import tensorflow as tf
 
 if __name__ == "__main__":
     training_data_path = "/Users/ivy/Desktop/Senior_Seminar/manuscript/training-data"
-    output_images_dir = "/Users/ivy/Desktop/Senior_Seminar/HOMUS-Bitmapifier/HOMUS_images"
+    output_images_dir = "/Users/ivy/Desktop/output"
     filepaths = [os.path.join(dp, f) for dp, dn, filenames in os.walk(training_data_path) for f in filenames if os.path.splitext(f)[1] == '.txt']
 
     symbols_list = list(map((lambda n: Symbol(n)), filepaths))
-
-    for symbol in symbols_list:
+    # print(symbols_list)
+    for symbol in symbols_list[0:1]:
         directory_to_write=f"{output_images_dir}/{symbol.name}/{symbol.filename}"
         print(f"Writing to: {directory_to_write}")
         symbol.draw(directory_to_write)
